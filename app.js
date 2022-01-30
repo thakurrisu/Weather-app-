067fa49deaff17b44724089083bfde5ec9498b0c
Rishikesh <rishikesh.kumar.921677@gmail.com>
2022-01-30 22:37:10 +0530

    Deployment

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const ejs = require("ejs");
+const fun = require("./data");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+//routes
+app.get("/", (req, res) => {
+  res.render("home"); // console.log('Homepage')
+});
+
+app.get("/weather", (req, res) => {
+  const city = req.query.city; //returns query string;
+  if (!city) return res.render("home");
+  fun(city, (err, data) => {
+    if (err) res.send(err.message);
+    else {
+      city_name = data.name;
+      temp = data.main.temp;
+      temp = temp - 273;
+      temp = Math.trunc(temp);
+
+      desec = data.weather[0].description;
+
+      res.render("city", { x: city_name, temp: temp, desec: desec });
+    }
+  });
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("NOT FOund");
+  console.log("invalid request");
+});
+
+app.listen(port, () => {
+  console.log("up and running");
+});
